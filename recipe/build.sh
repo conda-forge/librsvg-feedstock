3932b7767c0be0c8a68018f26739dd23f0b2394e
Kai Tietz <ktietz@anaconda.com>
2023-03-10 14:23:24 +0100

    attempt to create ./install

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,13 @@ export FGREP="grep -F"
 export GREP="grep"
 export MKDIR="mkdir"
 export MKDIR_P="mkdir -p"
+
+where=$(which "install" 2>/dev/null || true)
+
+if [ -n "${where}" ]; then
+  ln -s ${where} ./install
+fi
+
 export INSTALL="install"
 
 if [[ ${target_platform} == linux-ppc64le ]]; then
@@ -19,6 +26,7 @@ if [[ ${target_platform} == linux-ppc64le ]]; then
   cp -Rn powerpc64le-conda_cos7-linux-gnu/* powerpc64le-conda-linux-gnu/. || true
   popd
 fi
+
 
 export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH_FOR_BUILD}:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig
