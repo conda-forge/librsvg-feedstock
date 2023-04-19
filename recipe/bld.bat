27dda532fbb9eda1aefdac625e2b240fa07a2564
Peter Williams <peter@newton.cx>
2023-04-19 21:09:38 +0000

    recipe/bld.bat: tweak to fix the Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -24,6 +24,7 @@ set ^"LIBRSVG_OPTIONS=^
   INTROSPECTION=1 ^
   RUSTUP=echo ^
   LIBINTL_LIB="intl.lib iconv.lib advapi32.lib bcrypt.lib" ^
+  CARGO_CMD="cargo --locked build --release $(MANIFEST_PATH_FLAG) $(CARGO_TARGET_DIR_FLAG)" ^
  ^"
 
 :: configure files
