2321dbb938ebefdbcbee05b8733232a3cb80fd44
ClayWarren <warren.clay@icloud.com>
2026-09-14 15:12:13 -0500

    test: simplify package validation across Windows architectures

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -10,9 +10,3 @@ cmake --build consumer-build
 if errorlevel 1 exit /b 1
 consumer-build\rsvg-consumer.exe
 if errorlevel 1 exit /b 1
-set "EXPECTED_MACHINE=8664 machine (x64)"
-if "%target_platform%" == "win-arm64" set "EXPECTED_MACHINE=AA64 machine (ARM64)"
-for %%F in ("consumer-build\rsvg-consumer.exe" "%LIBRARY_BIN%\rsvg-convert.exe" "%LIBRARY_BIN%\rsvg-2-2.dll" "%LIBRARY_BIN%\rsvg-2.0-vs%VS_MAJOR%.dll" "%LIBRARY_LIB%\gdk-pixbuf-2.0\2.10.0\loaders\libpixbufloader_svg.dll") do (
-    dumpbin /headers "%%~F" | findstr /c:"%EXPECTED_MACHINE%"
-    if errorlevel 1 exit /b 1
-)
